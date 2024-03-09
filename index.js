7443e21580496eedb5c89943a77e60da85a26e31
Iliya Lyan <68940374+12ya@users.noreply.github.com>
2024-03-09 17:23:34 +0900

    server is ready + init frontend

--- index.js
+++ index.js
@@ -13,17 +13,49 @@ app.listen(PORT, () => {
 
 app.use(bodyParser.json());
 
-app.get('/issue/:id', (req, res) => {
+app.get('/issues/:id', (req, res) => {
     const { id: id_to_get } = req.params;
     console.log(`🚀 ---------------------🚀`);
     console.log(`🚀 ~ app.get ~ id:`, id_to_get);
-    const issue_to_get = issues.find((issue) => issue.id === id_to_get);
+    const issue_to_get = issues.find((issue) => {
+        console.log(`🚀 -------------------------------------------------🚀`);
+        console.log(`🚀 ~ constissue_to_get=issues.find ~ issue:`, issue);
+        console.log(`🚀 -------------------------------------------------🚀`);
+        return issue.id == id_to_get;
+    });
 
-    if (!issue_to_get) res.send('DONT EXIST');
-    res.send({ id: id_to_get });
+    // if (!issue_to_get) res.send('DONT EXIST');
+    res.send({ issue: issue_to_get });
 });
 
-app.post('/issue', (req, res) => {
+app.get('/issues', (req, res) => {
+    res.send({ issues });
+});
+
+app.post('/issues', (req, res) => {
     const { id, description, title } = req.body;
-    issues.push({ id, description, title });
+    issues.push({ id: Date.now(), description, title });
+    res.send(`Successfully created ${id} with title ${title}`);
+});
+
+app.delete('/issues', (req, res) => {
+    const { id } = req.body;
+    const issue_to_get = issues.find((issue) => issue.id == id);
+    if (!issue_to_get) res.send('DONT EXIST');
+
+    issues = issues.filter((issue) => issue.id != id);
+
+    res.send(`Successfully deleted ${id} with title ${issue_to_get.title}`);
+    res.send({ issues });
+});
+
+app.put('/issues', (req, res) => {
+    const { id, title, description } = req.body;
+
+    const issue_to_get = issues.find((issue) => issue.id == id);
+    if (!issue_to_get) res.send('DONT EXIST');
+
+    issues = issues.filter((issue) => issue.id != id);
+    issues.push({ ...issue_to_get, title, description });
+    res.send(`Successfully updated ${id} with title ${issue_to_get.title}`);
 });
